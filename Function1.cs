6e898969d1ce61cdcacbcd2a52a960bc43ca7b63
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-06 20:38:00 +0200

    Functions

--- Function1.cs
+++ Function1.cs
@@ -17,10 +17,20 @@ namespace FunctionApp28
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
         {
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-         
-
+            var data = JsonConvert.DeserializeObject<Book>(requestBody);
             string responseMessage = "";
+            if (data is null)
+            {
+                responseMessage = "no Book was sent to add, please add avalid book";
+                return new OkObjectResult(responseMessage);
+            }
+            Book book = new Book();
+            book.BookAuthor = data.BookAuthor;
+            book.BookName = data.BookName;
+            BookContext bookContext = new BookContext();
+            bookContext.Books.Add(book);
+            bookContext.SaveChanges();
+            responseMessage = $"Book written by {book.BookAuthor} has been added";
 
             return new OkObjectResult(responseMessage);
         }
